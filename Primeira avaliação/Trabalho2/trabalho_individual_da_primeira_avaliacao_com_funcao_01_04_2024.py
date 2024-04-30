25ec4834a3c4263ebde2e743bc93e3582d757dc8
ccfernandes600 <ccfernandes600@hotmail.com>
2024-04-29 21:03:12 -0300

    Nova correção de nomes de constantes e variáveis segundo a norma pep 8

--- Primeira avaliação/Trabalho2/trabalho_individual_da_primeira_avaliacao_com_funcao_01_04_2024.py
+++ Primeira avaliação/Trabalho2/trabalho_individual_da_primeira_avaliacao_com_funcao_01_04_2024.py
@@ -1,16 +1,23 @@
+""" programa para calcular a conta de energia elétrica de um
+consumidor residencial da Equatorial MA
+"""
 
 
 def obter_nome_cliente():
+    """ função para obter o nome do cliente"""
     print("\n\n\nBem vindo ao sistema de cálculo de conta de energia da Equatorial MA\n\n")
     return input("Entre com o nome do consumidor:  ")
 
-def obter_tipo_tarifa(): #tipo de tarifa de energia Residencial de Equatorial MA
+
+def obter_tipo_tarifa():  # tipo de tarifa de energia Residencial de Equatorial MA
+    """funcão para obter o tipo de tarifa de energia Residencial de Equatorial MA"""
     print("""MENU:
     [TBR] - Tarifa Baixa Renda
     [TRNC] - Tarifa Residencial Normal Convencional
     [TRNB] - Tarifa Residencial Normal Branca\n\n""")
     while True:
-        tipo_tarifa = input("Entre com um das opções \"TBR\",\"TRNC\" ou \"TRNB\" :")
+        tipo_tarifa = input(
+            "Entre com um das opções \"TBR\",\"TRNC\" ou \"TRNB\" :")
         tipo_tarifa = tipo_tarifa.upper()
         if tipo_tarifa in ["TBR", "TRNC", "TRNB"]:
             return tipo_tarifa
@@ -18,91 +25,105 @@ def obter_tipo_tarifa(): #tipo de tarifa de energia Residencial de Equatorial MA
             print("Opção de tarifa inválida")
             continue
 
-def obter_consumo_total(): #consumo total de energia em kWh
+
+def obter_consumo_total():  # consumo total de energia em kWh
+    """funcão para obter o consumo total de energia em kWh"""
     return float(input("Entre com o valor de kWh consumido no mês: "))
 
-def calcular_consumo(consumo_total,tipo_tarifa,forma_pagamento): #calcular o valor da conta de energia para cada tipo de tarifa de energia Residencial de Equatorial MA
-    
-    #Definição de variáveis e Constantes
-    
-    #Tarifa Residencial Baixa Renda
-    VALOR_consumo30K=0.23033 #Tarifa (R$/kWh)
-    VALOR_consumo31_100K=0.39485 #Tarifa (R$/kWh)
-    VALOR_consumo101_220K=0.59227 #Tarifa (R$/kWh)
-    VALOR_consumo_acima_220K=0.65808 #Tarifa (R$/kWh)
-    #Tarifa Residencial Normal Convencional
-    VALOR_consumo_convencional= 0.71881 #Tarifa (R$/kWh)
-    #Tarifa Residencial Normal Branca
-    VALOR_consumo_ponta=1.5968 #Tarifa (R$/kWh)
-    VALOR_consumo_intermediário=1.01266 #Tarifa (R$/kWh)
-    VALOR_consumo_fora_ponta=0.57891 #Tarifa (R$/kWh)
 
-    #inicialização de variáveis
-    valor_consumo30K = 0.0
-    valor_consumo31_100K = 0.0
-    valor_consumo101_220K = 0.0
-    valor_consumo_acima_220K = 0.0
-    
-    consumo30K = 0
-    consumo31_100K = 0
-    consumo101_220K = 0
-    consumo_acima_220K = 0
-    if tipo_tarifa == "TBR" :
+def calcular_consumo(consumo_total, tipo_tarifa, forma_pagamento):
+#calcular o valor da conta de energia para cada tipo de tarifa
+#de energia Residencial de Equatorial MA
+    """função para calcular o valor da conta de energia para cada tipo de tarifa de energia
+Residencial de Equatorial MA"""
+    # Definição de variáveis e Constantes
+
+    # Tarifa Residencial Baixa Renda
+    valor_consumo_30k = 0.23033  # Tarifa (R$/kWh)
+    valor_consumo_31_100k = 0.39485  # Tarifa (R$/kWh)
+    valor_consumo_101_220k = 0.59227  # Tarifa (R$/kWh)
+    valor_consumo_acima_220k = 0.65808  # Tarifa (R$/kWh)
+    # Tarifa Residencial Normal Convencional
+    valor_consumo_convencional = 0.71881  # Tarifa (R$/kWh)
+    # Tarifa Residencial Normal Branca
+    valor_consumo_ponta = 1.5968  # Tarifa (R$/kWh)
+    valor_consumo_intermediario = 1.01266  # Tarifa (R$/kWh)
+    valor_consumo_fora_ponta = 0.57891  # Tarifa (R$/kWh)
+
+    # inicialização de variáveis
+    valor_consumo_30k = 0.0
+    valor_consumo_31_100k = 0.0
+    valor_consumo_101_220k = 0.0
+    valor_consumo_acima_220k = 0.0
+
+    consumo_30k = 0
+    consumo_31_100k = 0
+    consumo_101_220k = 0
+    consumo_acima_220k = 0
+    if tipo_tarifa == "TBR":
         if consumo_total <= 30:
-            consumo30K = consumo_total
+            consumo_30k = consumo_total
         elif consumo_total > 30 and consumo_total <= 100:
-            consumo30K = 30
-            consumo31_100K = consumo_total - 30
+            consumo_30k = 30
+            consumo_31_100k = consumo_total - 30
         elif consumo_total > 100 and consumo_total <= 220:
-            consumo30K = 30
-            consumo31_100K = 70
-            consumo101_220K = consumo_total - 100
+            consumo_30k = 30
+            consumo_31_100k = 70
+            consumo_101_220k = consumo_total - 100
         else:
-            consumo30K = 30
-            consumo31_100K = 70
-            consumo101_220K = 120
-            consumo_acima_220K = consumo_total - 220
-        
-        #Calculo do consumo Baixa Renda C
-        valor_consumo30K=consumo30K * VALOR_consumo30K
-        valor_consumo31_100K = consumo31_100K * VALOR_consumo31_100K
-        valor_consumo101_220K= consumo101_220K * VALOR_consumo101_220K
-        valor_consumo_acima_220K= consumo_acima_220K * VALOR_consumo_acima_220K
+            consumo_30k = 30
+            consumo_31_100k = 70
+            consumo_101_220k = 120
+            consumo_acima_220k = consumo_total - 220
+
+        # Calculo do consumo Baixa Renda C
+        valor_consumo_30k = consumo_30k * valor_consumo_30k
+        valor_consumo_31_100k = consumo_31_100k * valor_consumo_31_100k
+        valor_consumo_101_220k = consumo_101_220k * valor_consumo_101_220k
+        valor_consumo_acima_220k = consumo_acima_220k * valor_consumo_acima_220k
         if forma_pagamento == "P":
-            valor_conta=valor_consumo30K + valor_consumo31_100K + valor_consumo101_220K + valor_consumo_acima_220K
+            valor_conta = valor_consumo_30k + valor_consumo_31_100k + \
+                valor_consumo_101_220k + valor_consumo_acima_220k
         elif forma_pagamento == "D":
-            valor_conta=(valor_consumo30K + valor_consumo31_100K + valor_consumo101_220K + valor_consumo_acima_220K) * 1.02
+            valor_conta = (valor_consumo_30k + valor_consumo_31_100k +
+                           valor_consumo_101_220k + valor_consumo_acima_220k) * 1.02
         elif forma_pagamento == "C":
-            valor_conta=(valor_consumo30K + valor_consumo31_100K + valor_consumo101_220K + valor_consumo_acima_220K) * 1.05
+            valor_conta = (valor_consumo_30k + valor_consumo_31_100k +
+                           valor_consumo_101_220k + valor_consumo_acima_220k) * 1.05
     elif tipo_tarifa == "TRNC":
-        #Calculo do consumo Normal
-        consumo_convencional=consumo_total
-        valor_consumo=consumo_convencional * VALOR_consumo_convencional
+        # Calculo do consumo Normal
+        consumo_convencional = consumo_total
+        valor_consumo = consumo_convencional * valor_consumo_convencional
         if forma_pagamento == "P":
-            valor_conta=valor_consumo
+            valor_conta = valor_consumo
         elif forma_pagamento == "D":
-            valor_conta=valor_consumo * 1.02
+            valor_conta = valor_consumo * 1.02
         elif forma_pagamento == "C":
-            valor_conta=valor_consumo * 1.05
-    elif  tipo_tarifa == "TRNB":
-        consumo_ponta=consumo_total *.25 #15% do consumo total
-        consumo_intermediário=consumo_total *.35 #25% do consumo total
-        consumo_fora_ponta=consumo_total *.40 #60% do consumo total
-        #Calculo consumo Normal
-        valor_consumo_ponta=consumo_ponta * VALOR_consumo_ponta
-        valor_consumo_intermediário=consumo_intermediário * VALOR_consumo_intermediário
-        valor_consumo_fora_ponta=consumo_fora_ponta *  VALOR_consumo_fora_ponta
+            valor_conta = valor_consumo * 1.05
+    elif tipo_tarifa == "TRNB":
+        consumo_ponta = consumo_total * .25  # 15% do consumo total
+        consumo_intermediário = consumo_total * .35  # 25% do consumo total
+        consumo_fora_ponta = consumo_total * .40  # 60% do consumo total
+        # Calculo consumo Normal
+        valor_consumo_ponta = consumo_ponta * valor_consumo_ponta
+        valor_consumo_intermediario = consumo_intermediário * valor_consumo_intermediario
+        valor_consumo_fora_ponta = consumo_fora_ponta * valor_consumo_fora_ponta
         if forma_pagamento == "P":
-            valor_conta=valor_consumo_ponta + valor_consumo_intermediário + valor_consumo_fora_ponta
+            valor_conta = valor_consumo_ponta + \
+                valor_consumo_intermediario + valor_consumo_fora_ponta
         elif forma_pagamento == "D":
-            valor_conta=(valor_consumo_ponta + valor_consumo_intermediário + valor_consumo_fora_ponta) * 1.02
+            valor_conta = (
+                valor_consumo_ponta + valor_consumo_intermediario + valor_consumo_fora_ponta) * 1.02
         elif forma_pagamento == "C":
-            valor_conta=(valor_consumo_ponta + valor_consumo_intermediário + valor_consumo_fora_ponta) * 1.05
+            valor_conta = (
+                valor_consumo_ponta + valor_consumo_intermediario + valor_consumo_fora_ponta) * 1.05
     return valor_conta
 
-def escolha_forma_pagamento(): #Escolha da forma de pagamento da conta de energia   
+
+def escolha_forma_pagamento():  # Escolha da forma de pagamento da conta de energia
+    """função para escolha da forma de pagamento da conta de energia"""
     print("""Escolha a forma de Pagamento
-        
+
     MENU:
 
     [P] - Pagamento com PIX sem juros
@@ -111,55 +132,63 @@ def escolha_forma_pagamento(): #Escolha da forma de pagamento da conta de energi
 
     [C] - Pagamento com Cartão de Crédito com juros de 5%\n\n""")
     while True:
-            forma_pagamento=input("Entre com \"P\" , \"D\" ou \"C\" : ")
-            if forma_pagamento.upper() == "P" or forma_pagamento.upper() == "D" or forma_pagamento.upper() == "C":
-                return forma_pagamento.upper()
-            else:
-                print("Opção inválida")
-                continue
-        
-      
+        forma_pagamento = input("Entre com \"P\" , \"D\" ou \"C\" : ")
+        forma_pagamento = forma_pagamento.upper()
+        if forma_pagamento in ["P", "D", "C"]:
+            return forma_pagamento
+        print("Opção inválida")
 
-def impressão_conta(tipo_tarifa, nome_cliente, forma_pagamento, valor_conta):
-    if forma_pagamento == "P" :
+
+def impressao_conta(tipo_tarifa, nome_cliente, forma_pagamento, valor_conta):
+    """funcão para impressão da conta de energia do consumidor"""
+    if forma_pagamento == "P":
         forma_pagamento = "PIX"
-    elif forma_pagamento == "D" :
+    elif forma_pagamento == "D":
         forma_pagamento = "DÉBITO"
     else:
         forma_pagamento = "CRÉDITO"
-    if tipo_tarifa == "TBR" :
+    if tipo_tarifa == "TBR":
         tipo_tarifa = "Tarifa Residencial Baixa Renda Convencional"
         print("\n\n")
-        print(f"Impressão da conta do Consumidor {nome_cliente} com tarifa tipo {tipo_tarifa} ")
-        print(f"O valor da conta é : R$ {valor_conta:.2f} com pagamento no {forma_pagamento}   ")
-    elif tipo_tarifa == "TRNC" :
+        print(
+            f"Impressão da conta do Consumidor {nome_cliente} com tarifa tipo {tipo_tarifa} ")
+        print(
+            f"O valor da conta é : R$ {valor_conta:.2f} com pagamento no {forma_pagamento}   ")
+    elif tipo_tarifa == "TRNC":
         tipo_tarifa = "Tarifa Residencial Normal Convencional"
         print("\n\n")
-        print(f"Impressão da conta do Consumidor {nome_cliente} com tarifa tipo {tipo_tarifa} ")
-        print(f"O valor da conta é : R$ {valor_conta:.2f} com pagamento no {forma_pagamento}   ")
+        print(
+            f"Impressão da conta do Consumidor {nome_cliente} com tarifa tipo {tipo_tarifa} ")
+        print(
+            f"O valor da conta é : R$ {valor_conta:.2f} com pagamento no {forma_pagamento}   ")
     elif tipo_tarifa == "TRNB":
         tipo_tarifa = "Tarifa Residencial Normal Branca"
         print("\n\n")
-        print(f"Impressão da conta do Consumidor {nome_cliente} com tarifa tipo {tipo_tarifa} ")
-        print(f"O valor da conta é : R$ {valor_conta:.2f} com pagamento no {forma_pagamento} ")
+        print(
+            f"Impressão da conta do Consumidor {nome_cliente} com tarifa tipo {tipo_tarifa} ")
+        print(
+            f"O valor da conta é : R$ {valor_conta:.2f} com pagamento no {forma_pagamento} ")
     else:
         print("Tipo de tarifa inválida")
 
+
 def main():
-    while True: #Loop para continuar ou não o programa
+    """função principal do programa"""
+    while True:  # Loop para continuar ou não o programa
         nome_cliente = obter_nome_cliente()
-        consumo_total=obter_consumo_total()
+        consumo_total = obter_consumo_total()
         tipo_tarifa = obter_tipo_tarifa()
-        forma_pagamento=escolha_forma_pagamento()
-        valor_conta=calcular_consumo(consumo_total,tipo_tarifa,forma_pagamento)
-        impressão_conta(tipo_tarifa, nome_cliente, forma_pagamento, valor_conta)
+        forma_pagamento = escolha_forma_pagamento()
+        valor_conta = calcular_consumo(
+            consumo_total, tipo_tarifa, forma_pagamento)
+        impressao_conta(tipo_tarifa, nome_cliente,
+                        forma_pagamento, valor_conta)
         continuar = input("Deseja continuar? [S/N]: ")
         if continuar.upper() == "N":
             print("Programa encerrado")
             break
         else:
             continue
-    
 
 
 if __name__ == "__main__":
